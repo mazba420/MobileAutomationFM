493645b80e0d231ac41f535afd32996fd948103c
mazba420 <mazba_420@yahoo.com>
2021-06-24 19:47:04 -0400

    -m "code added"

--- generic/src/main/java/com/mobile/testbase/TestBase.java
+++ generic/src/main/java/com/mobile/testbase/TestBase.java
@@ -1,4 +1,23 @@
 package com.mobile.testbase;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
 public class TestBase {
+
+    public void getAppiumDriver(){
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME,"R58N35NNPGA");
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        cap.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10");
+        cap.setCapability(MobileCapabilityType.APP_PACKAGE,"com.tdbank");
+        cap.setCapability(MobileCapabilityType.APP_ACTIVITY,"com.td.dcts.android.us.app.SplashScreenActivity");
+
+    }
 }
