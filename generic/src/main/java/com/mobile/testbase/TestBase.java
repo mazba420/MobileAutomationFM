a9aac7876494604c28fdecf0f1b38ae8c495bf62
mazba420 <mazba_420@yahoo.com>
2021-06-24 21:16:07 -0400

    code added

--- generic/src/main/java/com/mobile/testbase/TestBase.java
+++ generic/src/main/java/com/mobile/testbase/TestBase.java
@@ -5,19 +5,34 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class TestBase {
-
-    public void getAppiumDriver(){
+    public static AppiumDriver driver;
+    @Parameters({"platform","deviceName","platformVersion"})
+    @BeforeMethod
+    public void getAppiumDriver(String platform,String deviceName,String platformVersion) throws MalformedURLException {
         DesiredCapabilities cap = new DesiredCapabilities();
-        cap.setCapability(MobileCapabilityType.DEVICE_NAME,"R58N35NNPGA");
-        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
-        cap.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10");
-        cap.setCapability(MobileCapabilityType.APP_PACKAGE,"com.tdbank");
-        cap.setCapability(MobileCapabilityType.APP_ACTIVITY,"com.td.dcts.android.us.app.SplashScreenActivity");
+        if (platform.equalsIgnoreCase("android")) {
+            cap.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
+            cap.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+            cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, platformVersion);
+            cap.setCapability(MobileCapabilityType.APP_PACKAGE, "com.tdbank");
+            cap.setCapability(MobileCapabilityType.APP_ACTIVITY, "com.td.dcts.android.us.app.SplashScreenActivity");
+            driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), cap);
+        }else {
+            //ios device code
+        }
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+
 
     }
 }
---
+++ generic/src/test/java/com/tdbank/homepagetest/HomePageTest.java
@@ -0,0 +1,24 @@
+package com.tdbank.homepagetest;
+
+import com.mobile.testbase.TestBase;
+import org.testng.annotations.Test;
+
+public class HomePageTest extends TestBase {
+
+    @Test
+    public void validateAppIsLaunching(){
+        System.out.println(driver.getPageSource());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
